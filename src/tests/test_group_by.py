52385d2ee74b48c340d1aee7634e06b32517df3b
Ragy Abraham <ragy@rna.digital>
2022-07-17 09:21:00 -0700

    updated test_group_by
    added a unit test for the composite transform group by

--- src/tests/test_group_by.py
+++ src/tests/test_group_by.py
@@ -8,24 +8,34 @@ from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 import logging
 import unittest
+import json
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 
-def test_group_by():
-    options = PipelineOptions()
-    options.view_as(StandardOptions).streaming = True
-    with TestPipeline(options=options) as p:
-        output = (
-            p
-            |
-        )
+@beam.ptransform_fn
+def GroupBy(pcoll):
+    return (
+        pcoll
+        | 'Pair with One' >> beam.Map(lambda x: (x, 1))
+        | "Set Window" >> beam.WindowInto(beam.window.Sessions(1.0))
+        | "Group By Key" >> beam.GroupByKey()
+        | "Extract Message" >> beam.Map(lambda x: (x[0]))
+    )
 
-        assert_that(
-            output,
-            equal_to([])
-        )
+
+class GroupByKeyTest(unittest.TestCase):
+    def test_group_by(self):
+        options = PipelineOptions()
+        options.view_as(StandardOptions).streaming = True
+        with TestPipeline(options=options) as p:
+            data = p | beam.Create([json.dumps({"key": "value"})])
+            output = data | GroupBy()
+            assert_that(
+                output,
+                equal_to([json.dumps({"key": "value"})])
+            )
 
 
 if __name__ == '__main__':
